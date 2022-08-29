3c8f1b5cc15e1197a71083cc68764d3181041f04
Francis Maina <sebastianfrancix@gmail.com>
2022-08-29 12:37:18 +0300

    Modified the text content of the paragraph with the id#text

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+  text = document.getElementById("text");
+  text.textContent = "This is really cool!";
+});
+
+// console.log(
+//   "This console.log() fires when the index.js loads - before DOM contentLoaded is triggered"
+// );
